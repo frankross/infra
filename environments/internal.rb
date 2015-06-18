a86810dc63e8a3d1caf55a9fbe797c9f31eb09f5
sharmavipul <vipulsharma2190@gmail.com>
2015-06-18 13:54:58 +0530

    fixed nat server

--- environments/internal.rb
+++ environments/internal.rb
@@ -3,6 +3,8 @@ description 'internal environment file'
 
 override_attributes(
   :vpc => {
-    :cidr => "10.60.0.0/16"
+    :cidr => "10.60.0.0",
+    :netmask => "255.255.0.0",
+    :nat_netmask => "16"
   }
 )
