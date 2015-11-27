5bb7169ad18e84e8c09d89f8a3c8c4a0b55e0bc1
Ershad Kunnakkadan <ershad92@gmail.com>
2015-11-27 18:59:34 +0530

    Changing bucket name

--- cookbooks/library/attributes/default.rb
+++ cookbooks/library/attributes/default.rb
@@ -1,7 +1,7 @@
 default['apps']['max_number_of_release_dirs'] = 5
 default["apps"]["user"]                       = "deploy"
 default["apps"]["group"]                      = "deploy"
-default["ci"]["bucket"]                       = "s3://emami-ci-packages"
+default["ci"]["bucket"]                       = "s3://emami-ci-packages-2"
 default.ruby[:bin_location]                   = "/usr/bin"
 default["chef_server"]["url"]                 = "https://chef-server.frankross.in/organizations/emami"
 default["ruby"]["s3_location"]                = "s3://emami-ci-packages/ruby/ruby-2.2.2-2_amd64.deb"
