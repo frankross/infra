22bd07ada76ad3349372ab04834af13ccf2467b2
Ershad Kunnakkadan <ershad92@gmail.com>
2015-11-29 10:38:55 +0530

    Fixing issues with TeamCity cookbook

--- cookbooks/teamcity-ci/recipes/server.rb
+++ cookbooks/teamcity-ci/recipes/server.rb
@@ -1,5 +1,5 @@
-include_recipe "teamcity-ci::_common"
 include_recipe "teamcity::server"
+include_recipe "teamcity-ci::_common"
 
 node.override["datadog"]["tags"].push("teamcity")
 process_check "teamcity" do
