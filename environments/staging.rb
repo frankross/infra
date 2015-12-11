a335beac8690252b6a0da0bf6b38f94dd1833f08
Ershad Kunnakkadan <ershad92@gmail.com>
2015-12-11 16:11:43 +0530

    Updating versions of ecom-platform

--- environments/staging.rb
+++ environments/staging.rb
@@ -3,7 +3,7 @@ description 'staging environment file'
 
 cookbook_versions ({
   'base'                => '= 0.0.7',
-  'ecom-platform'       => '= 0.1.29',
+  'ecom-platform'       => '= 0.1.30',
   'ecom-docs'           => '= 0.1.7',
   'emr'                 => '= 0.1.5',
   'library'             => '= 0.0.31',
