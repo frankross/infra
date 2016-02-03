fdc258215d776c64d3d6fd6b6771c662b75eb5b9
Rahul Mahale <rahul.mahale123@gmail.com>
2016-02-03 16:13:43 +0530

    Fixin logrotate for delayed_job

--- environments/staging.rb
+++ environments/staging.rb
@@ -3,7 +3,7 @@ description 'staging environment file'
 
 cookbook_versions ({
   'base'                => '= 1.0.0',
-  'ecom-platform'       => '= 1.0.1',
+  'ecom-platform'       => '= 1.0.2',
   'ecom-docs'           => '= 0.1.8',
   'emr'                 => '= 0.1.7',
   'library'             => '= 1.0.0',
