3a45989b3683c45edc41a670060c38a6fc9026f8
Rahul Mahale <rahul.mahale123@gmail.com>
2016-03-09 11:54:37 +0530

    delayed job recipes for different job servers

---
+++ cookbooks/ecom-platform/recipes/delayed_job_2.rb
@@ -0,0 +1,82 @@
+#
+# Cookbook Name:: delayed_job
+# Recipe:: default
+#
+app                                  = "ecom-platform"
+app_location                         = node.apps.location
+app_user                             = node.apps[:user]
+app_group                            = node.apps[:group]
+
+node.override["apps"]["init_script_name"] = "dj"
+node.default["monitoring"]["processes"]  = [{name: 'delayed_job', search_string: ['delayed_job']}]
+
+include_recipe "ecom-platform::_common"
+
+app_environment_variables = {}
+app_environment_variables.merge! node["ecom-platform"].environment_variables
+
+link_release "ecom-platform" do
+  app_location node.apps.location
+  app_service node.apps.init_script_name
+end
+
+execute "create delayed job bin" do
+  command "bundle exec rails generate delayed_job"
+  env Hash[app_environment_variables.merge("RAILS_ENV" => "production").map{|key, value| [ key.to_s, value.to_s ]}]
+  cwd "#{app_location}/current"
+  not_if { ::File.exists?("#{app_location}/current/bin/delayed_job") }
+end
+
+worker_queue_map = {
+  :upload => 2,
+  :download => 1,
+  :default => 2
+}
+
+queue_config = ""
+worker_queue_map.each do |key, value|
+  queue_config << " --pool=#{key}:#{value}"
+end
+
+template "/etc/init.d/dj" do
+  source "dj/dj.erb"
+  variables(app: app,
+            user: app_user,
+            group: app_group,
+            queue_config: queue_config,
+            env: node.chef_environment,
+            project_path: app_location)
+  mode "775"
+  owner app_user
+  group app_group
+  notifies:restart, "service[dj]", :delayed
+end
+
+service "dj" do
+  supports status: true, start: true, stop: true, restart: true
+  action :enable
+end
+
+['libxslt1.1','libxslt1-dev','libpcre3-dev',"imagemagick","libmagickwand-dev","build-essential"].each do |pkg|
+  log 'message' do
+    message "Installing package #{pkg}"
+    level :debug
+  end
+  package pkg
+end
+
+node.override["datadog"]["tags"].push("delayed_job")
+
+process_check "ecom-platform" do
+  process node["monitoring"]["processes"]
+end
+
+papertrail "app" do
+  log_file ["#{app_location}/current/log/*.log","#{app_location}/shared/log/*.log","/var/log/syslog"]
+end
+
+_logrotate "app_logs" do
+    path "#{app_location}/shared/log/*.log"
+  end
+
+monit "dj"
--- environments/staging.rb
+++ environments/staging.rb
@@ -3,7 +3,7 @@ description 'staging environment file'
 
 cookbook_versions ({
   'base'                => '= 1.0.0',
-  'ecom-platform'       => '= 1.0.4',
+  'ecom-platform'       => '= 1.0.6',
   'ecom-docs'           => '= 0.1.8',
   'emr'                 => '= 0.1.7',
   'library'             => '= 1.0.0',
