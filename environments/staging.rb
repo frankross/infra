05a55bbfcfaab6a91379527b34e74ef599d72e0c
Midhun Krishna <kr1shn4.m1dhun@gmail.com>
2016-03-29 16:36:39 +0530

    FR-1545 update metadata for ecom staging

--- environments/staging.rb
+++ environments/staging.rb
@@ -4,7 +4,7 @@ description 'staging environment file'
 cookbook_versions ({
   'base'                => '= 1.0.0',
   'ecom-platform'       => '= 1.0.6',
-  'ecom-docs'           => '= 0.1.8',
+  'ecom-docs'           => '= 0.1.9',
   'emr'                 => '= 0.1.8',
   'library'             => '= 1.0.0',
   'proxy'               => '= 1.0.0'
