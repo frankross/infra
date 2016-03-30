4410efd14454d4ee950acd3f3f2e3379919e36a1
Rahul Mahale <rahul.mahale123@gmail.com>
2016-03-30 10:55:16 +0530

    Adding new cron job

--- cookbooks/ecom-platform/metadata.rb
+++ cookbooks/ecom-platform/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures ecom-platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.0.8'
 
 depends "library"
 depends "base"
